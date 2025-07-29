7e76e5d28cc8f96df77f7b4abc7422b9436ea3f8
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2025-07-29 05:26:27 -0400

    Create Taking Input and Output In Arrays.cpp

---
+++ Taking Input and Output In Arrays.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[10];
+    int x; 
+    
+    for(int i =0;i<10;i++){
+        cout<<"enter the numbers of an array : ";
+        cin>>arr[i]; //Taking the input
+    }
+    cout<<"the array entered is : "<< endl;
+    for(int i =0;i<10;i++){
+        
+        cout<<arr[i]; //Giving the output
+        cout<< " ";
+    }
+    cout<<endl;
+    return 0;
+}
+
+/* Output :
+enter the numbers of an array : 1
+enter the numbers of an array : 2
+enter the numbers of an array : 3
+enter the numbers of an array : 4
+enter the numbers of an array : 5
+enter the numbers of an array : 6
+enter the numbers of an array : 7
+enter the numbers of an array : 8
+enter the numbers of an array : 9
+enter the numbers of an array : 10
+the array entered is : 
+1 2 3 4 5 6 7 8 9 10 
+*/
