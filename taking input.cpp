5269af74928ae9421059d8e7574b76ddf5b1187c
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2025-08-03 17:11:22 +0530

    Create taking input.cpp

---
+++ taking input.cpp
@@ -0,0 +1,32 @@
+//EXPERIMENT-7
+// MAITRAIYEE VASHISTHA
+// 24070123057
+// ENTC A3
+#include <iostream>
+using namespace std;
+
+int main() {
+int marks[6];
+cout<<"The marks of student are:"<<endl;
+
+for(int i=0; i<6; i++){
+    cin>>marks[i];
+}
+
+for(int n=0; n<6; n++){
+    cout<<marks[n]<<" ";
+}
+
+   return 0;
+}
+
+/*output:-
+The marks of student are:
+99
+89
+78
+58
+68
+85
+99 89 78 58 68 85 
+*/
