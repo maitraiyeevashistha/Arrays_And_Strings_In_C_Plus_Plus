f7e2e7c002b578a2d8b029edf12f33f3367ee8a4
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2025-08-03 17:07:40 +0530

    Create concatenation of strings.cpp

---
+++ concatenation of strings.cpp
@@ -0,0 +1,32 @@
+//EXPERIMENT-7
+// MAITRAIYEE VASHISTHA
+// 24070123057
+// ENTC A3
+#include <iostream>
+using namespace std;
+int main() {
+
+    string str1;
+    string str2;
+    cout<<"Enter the string:"<<endl;
+    cin>>str1;
+    
+    cout<<"Enter the string:"<<endl;
+    cin>>str2;
+    
+    string str;
+    str= str1+  " " + str2;
+    cout<<"The concatenated string is: "<<str;
+    
+    return 0;
+}
+
+
+/*
+Output:-
+Enter the string:
+Maitraiyee 
+Enter the string:
+Vashistha
+The concatenated string is: Maitraiyee Vashistha
+*/
